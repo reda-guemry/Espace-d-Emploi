3ee4a8c04f91f60d083dc03988cfe237b336d7ca
reda-guemry <guemryreda@gmail.com>
2026-02-02 15:17:31 +0000

    modifier the moddels

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Application extends Model
 {
-    //
+    protected $fillable = [
+        'job_id' , 
+        'user_id' , 
+        'cover_letter' , 
+        'cv_attached' , 
+
+    ] ;
+
+
+    public function user (): BelongsTo  
+    {
+        return $this -> belongsTo(User::class) ;
+    }
+
+    public function job(): BelongsTo 
+    {
+        return $this -> belongsTo(Job::class) ;
+    }
+
 }
--- app/Models/Education.php
+++ app/Models/Education.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Education extends Model
 {
-    //
+    protected $fillable = [
+        'user_id' 
+    ] ;
 }
